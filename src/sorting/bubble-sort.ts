621982e628056b10ea24914bb6ec002d9fdfd88a
nevinedwin <nevinedwin100@gmail.com>
2026-01-23 13:47:11 +0530

    testing

--- src/sorting/bubble-sort.ts
+++ src/sorting/bubble-sort.ts
@@ -1,11 +1,13 @@
 
 
-function bubbleSort(numArr: number[]) {
+function bubbleSort(numArr: number[], order: string = 'asc') {
 
     const lastIndex: number = numArr.length - 1;
 
     for (let i: number = 0; i < numArr.length; i++) {
         for (let j: number = lastIndex; j > i; j--) {
+            const condition = order === 'asc'   
+            
             if (numArr[j] < numArr[j - 1]) {
                 [numArr[j], numArr[j - 1]] = [numArr[j - 1], numArr[j]];
             }
